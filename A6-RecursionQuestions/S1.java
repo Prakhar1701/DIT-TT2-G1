89381b2653209df8137f0d0b4edd8ad1ecedc5b0
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-06-13 21:20:27 +0530

    Solved  A6-RecursionQuestions/Q1..Q2 :)

---
+++ A6-RecursionQuestions/S1.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/power-of-three/description/
+
+public class S1 {
+
+    public boolean isPowerOfFour(int n) {
+
+        if (n < 1) {
+            return false;
+        } else if (n == 1) {
+            return true;
+        }
+
+        if (!(n % 3 == 0)) {
+            return false;
+        }
+
+        return isPowerOfFour(n / 3);
+    }
+
+    // Using Iteration:
+
+    // public boolean isPowerOfThree(int n) {
+
+    // while (true) {
+
+    // if (n < 1) {
+    // return false;
+    // } else if (n == 1) {
+    // return true;
+    // }
+
+    // if (n % 3 == 0) {
+    // n = n / 3;
+    // } else {
+    // return false;
+    // }
+
+    // }
+
+    // }
+
+}
