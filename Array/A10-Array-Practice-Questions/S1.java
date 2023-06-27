06a12ad315f23c7108d19dc1753c021ae6b70827
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-06-27 14:41:00 +0530

    Solved "Rotate Array" by method-2

--- Array/A10-Array-Practice-Questions/S1.java
+++ Array/A10-Array-Practice-Questions/S1.java
@@ -2,20 +2,50 @@
 
 public class S1 {
 
+    // Method-2:
+    // Time Complexity : O(n)
+    // Space Complexity : O(n)
     public void rotate(int[] nums, int k) {
 
-        for (int j = 0; j < k; j++) {
+        int n = nums.length;
+        int[] ary = new int[n];
 
-            for (int i = nums.length - 1; i > 0; i--) {
+        while (k >= n) {
+            k -= n;
+        }
 
-                int t = nums[i];
+        for (int i = 0; i < nums.length; i++) {
 
-                nums[i] = nums[i - 1];
-                nums[i - 1] = t;
+            if (i + k >= n) {
+                ary[i + k - n] = nums[i];
+            } else {
+                ary[i + k] = nums[i];
             }
+        }
 
+        for (int i = 0; i < n; i++) {
+            nums[i] = ary[i];
         }
 
     }
+
+    // Method-1: (Time Limit Exceeded)
+    // Time Complexity : O(n^2)
+    // Space Complexity : O(1)
+    // public void rotate(int[] nums, int k) {
+
+    // for (int j = 0; j < k; j++) {
+
+    // for (int i = nums.length - 1; i > 0; i--) {
+
+    // int t = nums[i];
+
+    // nums[i] = nums[i - 1];
+    // nums[i - 1] = t;
+    // }
+
+    // }
+
+    // }
 
 }
