9a281e917099c3cce86a0e35646ffb9ca8dfd35a
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-07-14 23:11:54 +0530

    Solved "Reverse a linked list" :)

---
+++ LinkedList-Practice-Questions/S5.java
@@ -0,0 +1,52 @@
+// https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+public class S5 {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    // Function to reverse a linked list.
+    // Iterative Approch:
+    Node reverseList(Node head) {
+
+        Node p = null; // Previous
+        Node c = head; // Current
+        Node n = head; // Next
+
+        while (c != null) {
+            n = n.next;
+            c.next = p;
+            p = c;
+            c = n;
+        }
+
+        return p;
+    }
+
+    // Recursive Approch:
+    // Node reverseList(Node head) {
+
+    // head = reverse(null, head, head);
+
+    // return head;
+    // }
+
+    // Node reverse(Node p, Node c, Node n) {
+    // if (c != null) {
+    // n = n.next;
+    // c.next = p;
+    // p = c;
+    // c = n;
+    // } else {
+    // return p;
+    // }
+
+    // return reverse(p, c, n);
+    // }
+}
