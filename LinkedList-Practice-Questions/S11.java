176c86f30f7f40550173909c96958c2b4aa3fd86
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-07-17 00:25:45 +0530

    Solved "Copy List with Random Pointer" :)

--- LinkedList-Practice-Questions/S11.java
+++ LinkedList-Practice-Questions/S11.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/copy-list-with-random-pointer/description/
 
-import java.util.HashMap;
+// import java.util.HashMap;
 
 public class S11 {
 
@@ -18,36 +18,83 @@ public class S11 {
     }
 
     class Solution {
-        public Node copyRandomList(Node head) {
 
-            HashMap<Node, Node> map = new HashMap<>();
+        public Node copyRandomList(Node head) {
 
             Node t = head;
 
             while (t != null) {
 
                 Node cn = new Node(t.val);
+                cn.next = t.next;
+                t.next = cn;
 
-                map.put(t, cn);
-
-                t = t.next;
+                t = t.next.next;
             }
 
             t = head;
 
             while (t != null) {
 
-                Node cn = map.get(t);
+                t.next.random = (t.random == null) ? null : t.random.next;
 
-                cn.next = map.get(t.next);
-
-                cn.random = map.get(t.random);
-
-                t = t.next;
+                t = t.next.next;
             }
 
-            return map.get(head);
+            Node deepCopyHead = (head == null) ? null : head.next;
+
+            t = head;
+
+            Node ct = deepCopyHead;
+
+            while (t != null) {
+
+                t.next = t.next.next;
+                t = t.next;
+
+                if (t == null)
+                    continue;
+
+                ct.next = ct.next.next;
+                ct = ct.next;
+
+            }
+
+            return deepCopyHead;
         }
     }
+
+    // class Solution {
+    // public Node copyRandomList(Node head) {
+
+    // HashMap<Node, Node> map = new HashMap<>();
+
+    // Node t = head;
+
+    // while (t != null) {
+
+    // Node cn = new Node(t.val);
+
+    // map.put(t, cn);
+
+    // t = t.next;
+    // }
+
+    // t = head;
+
+    // while (t != null) {
+
+    // Node cn = map.get(t);
+
+    // cn.next = map.get(t.next);
+
+    // cn.random = map.get(t.random);
+
+    // t = t.next;
+    // }
+
+    // return map.get(head);
+    // }
+    // }
 
 }
