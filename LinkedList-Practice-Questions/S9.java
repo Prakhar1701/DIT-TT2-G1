3dde98008fb08fecd3588bf36fb397a8f7b295e7
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-07-16 14:51:23 +0530

    Solved "Add Two Numbers" :)
    Given two non-empty linked lists representing two non-negative integers.
    The digits are stored in reverse order, and each of their nodes contains a single digit.
    Add the two numbers and return the sum as a linked list.

--- LinkedList-Practice-Questions/S9.java
+++ LinkedList-Practice-Questions/S9.java
@@ -1,5 +1,9 @@
 // https://leetcode.com/problems/add-two-numbers/description/
 
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
 public class S9 {
 
     // Definition for singly-linked list.
@@ -20,11 +24,39 @@ public class S9 {
         }
     }
 
-    // Input: l1 = [2,4,3], l2 = [5,6,4]
-    // Output: [7,0,8]
-    // Explanation: 342 + 465 = 807.
+    class Solution1 {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode dummyHead = new ListNode(0);
+            ListNode current = dummyHead;
+            int carry = 0;
 
-    class Solution {
+            while (l1 != null || l2 != null) {
+                int sum = carry;
+
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                carry = sum / 10;
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+            }
+
+            if (carry > 0) {
+                current.next = new ListNode(carry);
+            }
+
+            return dummyHead.next;
+        }
+    }
+
+    class Solution2 {
 
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
